6503519edf2583751f0335f3b4faff074fac6183
Jonathan Mera <jonypachecomera@gmail.com>
2026-06-08 22:23:47 -0300

    Polymorphism example

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,25 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+static void Main(string[] args)
+{
+   Square square = new Square("Red", 5);
+
+   Console.WriteLine(square.GetColor());
+   Console.WriteLine(square.GetArea());
+
+   List<Shape> shapes = new List<Shape>();
+
+  shapes.Add(new Square("Blue", 4));
+  shapes.Add(new Rectangle("Green", 5, 3));
+  shapes.Add(new Circle("Yellow", 2));
+
+  foreach (Shape shape in shapes)
+{
+    Console.WriteLine(shape.GetColor());
+    Console.WriteLine(shape.GetArea());
+}
     }
 }
